1865c0f850850082d6baa6aee563f60e39236c15
Octave Dubois <octave.dubois@hotmail.fr>
2023-12-11 20:23:06 +0100

    enregistrement des données

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,13 +17,16 @@ class GamesController < ApplicationController
 
   def edit
     @game = Game.find(params[:id])
+    @game.players.build if @game.players.empty?
   end
+
 
   def update
     @game = Game.find(params[:id])
     if @game.update(game_params)
       redirect_to edit_game_path(@game), notice: 'La partie a été mise à jour avec succès.'
     else
+      @game = Game.find(params[:id])
       render 'edit'
     end
   end
@@ -32,6 +35,6 @@ class GamesController < ApplicationController
   private
 
   def game_params
-    params.require(:game).permit(:name, players_attributes: [:id, :name, :one, :two, :four, :five, :six, :triple, :quadriple, :full, :small, :long, :chance, :yams, :_destroy])
+    params.require(:game).permit(:name, players_attributes: [:id, :name, :one, :two, :three, :four, :five, :six, :triple, :quadriple, :full, :small, :long, :chance, :yams, :_destroy])
   end
 end
