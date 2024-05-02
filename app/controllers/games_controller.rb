5b4f41fe4cae9ba3f0ec88d80a0e6627ea8d2763
Octave Dubois <octave.dubois@hotmail.fr>
2024-05-02 13:21:32 +0200

    Ajout des scrores en fin de partie, ajout de la route + retravaille du front

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,19 +22,40 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
+
     if @game.update(game_params)
-      redirect_to edit_game_path(@game), notice: 'La partie a été mise à jour avec succès.'
+      if params[:game][:check_fields] && all_fields_filled?(@game)
+        # Si tous les champs sont remplis, rediriger vers la page des scores
+        redirect_to scores_game_path(@game)
+        return  # Ajoutez return pour s'assurer qu'aucun autre rendu ou redirection ne se produira après ceci
+      else
+        # Si les champs ne sont pas tous remplis, rediriger vers la même page d'édition avec un message
+        redirect_to edit_game_path(@game), notice: 'La partie a été mise à jour avec succès.'
+        return
+      end
     else
-      @game = Game.find(params[:id])
+      # Si la mise à jour échoue, rendre la vue 'edit' avec les objets actuels
       render 'edit'
+      return
     end
   end
 
+  def scores
+    @game = Game.find(params[:id])
+    # Vous pouvez ici charger d'autres données nécessaires pour afficher les scores
+  end
 
   private
 
   def game_params
     params.require(:game).permit(:name, players_attributes: [:id, :_destroy, :name, :one, :two, :three, :four, :five, :six, :triple, :quadriple, :full, :small, :long, :chance, :yams, :_destroy])
+  end
+
+  def all_fields_filled?(game)
+    game.players.all? do |player|
+      # Assurez-vous que tous les champs requis sont non nils et non vides
+      [player.one, player.two, player.three, player.four, player.five, player.six, player.triple, player.quadriple, player.full, player.small, player.long, player.chance, player.yams].all?
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "games#new"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  Rails.application.routes.draw do
-    resources :games, only: [:new, :create, :edit, :update] do
-      resources :players, only: [:new, :create]
+  resources :games, only: [:new, :create, :edit, :update] do
+    member do
+      get 'scores'  # Cette ligne ajoute la route pour voir les scores d'un jeu spécifique
     end
+    resources :players, only: [:new, :create]
   end
-
-
 end
