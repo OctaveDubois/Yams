33a8f3bc4724b202750a406ca0f0182ec9f7b9e4
Octave Dubois <octave.dubois@hotmail.fr>
2024-05-11 11:27:30 +0200

    Ajout de js pour le changement d'onglets

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,16 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+document.addEventListener("turbolinks:load", function() {
+  // Réinitialisation des onglets
+  $('#playerTabs a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+  // Activez l'onglet du joueur actif ici, en fonction de la logique que vous souhaitez
+});
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
