ac5373002481f5fe0f24f9516b73cdfe19e98120
Octave Dubois <octave.dubois@hotmail.fr>
2024-05-11 12:05:52 +0200

    ajout js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,15 +2,10 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-document.addEventListener("turbolinks:load", function() {
-  // Réinitialisation des onglets
-  $('#playerTabs a').on('click', function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
+if (typeof bootstrap === 'undefined') {
+  console.error('Bootstrap is not loaded');
+}
 
-  // Activez l'onglet du joueur actif ici, en fonction de la logique que vous souhaitez
-});
 
 // Configure Stimulus development experience
 application.debug = false
